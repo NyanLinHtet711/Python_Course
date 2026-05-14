47e0b29d5b3ff4afc4f0e2a612bd587e5b06fd62
NyanLinHtet <u6430031@au.edu>
2026-05-15 03:17:00 +0700

    Last Assignment

--- class_11_recursion/myOwnFun.py
+++ class_11_recursion/myOwnFun.py
@@ -2,17 +2,31 @@ def SplitType(numlist):
     fl=[]
     il=[]
     for i in numlist:
-        if i.isfloat():
-            fl.append(i)
-        else:
+        if isinstance(i,int):
             il.append(i)
+        else:
+            fl.append(i)
     return fl,il 
 def oddlist(numlist):
-    ol=[]
+   
     if len(numlist)==0:
         return []
     if numlist[0]%2!=0:
         return [numlist[0]]+oddlist(numlist[1:])
-    return oddlist[1:]
+    return oddlist(numlist[1:])
+
+def dul_int(numlist): # [1,2,3,3,4,5,4]
+    dlist=[]
+    seen1=[]
+    seen2=[]
+    for i in numlist:
+        if i not in seen1:
+            seen1.append(i)
+            dlist.append(i)
+        elif i not in seen2:
+            seen2.append(i)
+            dlist.append(i)
+    return dlist
+
 
 
---
+++ class_11_recursion/test_myownFun.py
@@ -0,0 +1,9 @@
+import myOwnFun
+
+numlist = [1,2,3,3,4,5,4,2.5]
+
+print(myOwnFun.SplitType(numlist))
+
+print(myOwnFun.oddlist(numlist))
+
+print(myOwnFun.dul_int(numlist))
